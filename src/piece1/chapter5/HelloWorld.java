74d2d42c27635a55bc458429a49fe64161dfa31f
404NoFound <xsxiaosa@gmail.com>
2013-06-06 20:47:15 +0800

    First  HelloWorld

---
+++ src/piece1/chapter5/HelloWorld.java
@@ -0,0 +1,13 @@
+package piece1.chapter5;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO 自动生成的方法存根
+	System.out.println("HelloWord");
+    }
+
+}
