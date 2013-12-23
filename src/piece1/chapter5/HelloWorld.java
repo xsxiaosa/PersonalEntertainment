4a3f605a091398624333c6ca0e645af8af80a0ba
xsxiaosa <xsxiaosa@gmail.com>
2013-12-23 09:40:38 +0800

    Idea test

--- src/piece1/chapter5/HelloWorld.java
+++ src/piece1/chapter5/HelloWorld.java
@@ -8,6 +8,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 	// TODO 自动生成的方法存根
 	System.out.println("HelloWord");
+
+        for(int i=0;i<10;i++){
+            double rand=Math.random()*10+1;
+            System.out.println(rand);
+        }
     }
 
 }
